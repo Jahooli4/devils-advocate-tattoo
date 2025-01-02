338482046fba078c4ce234d0b4ec3745c7f82f3c
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2025-01-02 12:16:25 +0000

    feat: create template directory and html files

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='booking-home',)
+    path('', views.home, name='booking-home'),
+    path('about/', views.about, name='booking-about'),
 ]
